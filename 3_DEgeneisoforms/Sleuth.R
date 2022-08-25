fd094b2f00a42e3cd0808690cd54005b300d803f
OierAzur <111640489+OierAzur@users.noreply.github.com>
2022-08-25 22:50:08 +0200

    Update Sleuth.R

--- 3_DEgeneisoforms/Sleuth.R
+++ 3_DEgeneisoforms/Sleuth.R
@@ -1,7 +1,7 @@
 #Trabajo Fin de Máster - Máster en Métodos Computacionales en ciencias
 #Autor: Oier Azurmendi Senar
 #Tutores_ Silvestre Vicent Cambra y Mikel Hernaez
-#Curso 2022-2023
+#Curso 2021-2022
 
 #Objetivo: Primera observación preliminar de las muestras y condiciones, y análisis de expresión 
 #diferencial de los isoformas de los genes. 
